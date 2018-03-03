c0af01a034291d5d2033d821d40adaea9b5835ff
Miika Sikala <miikasikala96@gmail.com>
2018-03-03 22:42:58 +0200

    Added array_unset helper

--- app/Framework/helpers.php
+++ app/Framework/helpers.php
@@ -1,8 +1,21 @@
 <?php
 
-// TODO: config('app.locale'), config('paths.templates') etc...
-// no need to relaod the whole thing every time, right?
-function config($config = '')
+/*---------------------------------------------------------
+ * Alpaca helpers
+ *---------------------------------------------------------
+ *
+ * This file contains general helper function used by Alpaca.
+ * You are free to use them as well but if you wish to create
+ * your own helper functions, add new one to app folder.
+ */
+
+/**
+ * @todo Not permanent! Is this even ok ciz we load this every time from drive??
+ * @todo config('app.locale'), config('paths.templates') etc...
+ * @param string $config Config file's name matching one in app/config
+ * @return array Config file as array if found
+ */
+function config(string $config = '') : array
 {
 	$configPath = PATH_ROOT . '/config/' . $config . '.php';
 
@@ -11,4 +24,30 @@ function config($config = '')
 		: [];
 
 	return $configFile;
+}
+
+/**
+ * Get data value from array. Checks if stuff exists for you.
+ *
+ * @todo Put this to main Controller
+ * @param array $params parameter array which Router passed to the method
+ * @return Return the value from the array or null if not found
+ */
+function data($params = [], $key = '')
+{
+	return $params[$key] ?? null;
+}
+
+
+/**
+ * Unset element from array and return reindexed array
+ *
+ * @param array $array 
+ * @param integer $index Which element to remove
+ * @return array
+ */
+function array_unset(array &$array, $index = 0) : array
+{
+	unset($array[$index]);
+	return array_values($array);
 }
