7fe1647a64f1a3aa9a2bc6361252863525da2e4b
Miika Sikala <miikasikala96@gmail.com>
2018-03-06 20:00:18 +0200

    Refactored RouteMatcher

--- app/Framework/Libs/RouteMatcher.php
+++ app/Framework/Libs/RouteMatcher.php
@@ -92,9 +92,11 @@ class RouteMatcher
 			$paramClause = $this->regex('something');
 
 			// Parse request method
-			$method = $this->getMethod($url);
+			$method = $this->getMethodFromRoute($url);
 
-			$url = trim($url, "/");
+			// Trim stuff
+			$url = trim($url, '/');
+			$url = str_replace(' ', '', $url);
 
 			// extract parameter keys from route for later use
 			$params = $this->extractParts($this->regex('brackets'), $url)[0];
@@ -123,33 +125,11 @@ class RouteMatcher
 			$regexRoutes["/^\/{$url}$/"] = [
 				'method' => $method,
 				'action' => $action,
-				'paramKeys' => $params
+				'paramNames' => $params
 			];
 		}
 		
 		return $regexRoutes;
-	}
-	protected function getMethod(string $url) : string
-	{
-		preg_match_all($this->regex('method'), $url, $results);
-
-		if (isset($results[1]) && isset($results[1][0])) {	
-			return  $this->getMethodByKey($results[1][0]);
-		}
-
-		throw new InternalException("Method missing from route definiton {$url}.");
-	}
-	protected function getMethodByKey(string $key)
-	{
-		$methods = [
-			'&' => 'GET',
-			'$' => 'POST'
-		];
-
-		if(array_key_exists($key, $methods))
-			return $methods[$key];
-		else
-			return false;
 	}
 
 	/**
@@ -166,29 +146,32 @@ class RouteMatcher
 		$result = 	['','',[]];
 
 		foreach ($routes as $match => $action) {
-			if (preg_match($match, $url) && $this->method == $action['method']) {
 
+			if ($this->isMatchingUrl($match, $url, $action['method'])) {
 				// if we have a match, let's build an action for it
 				$params = $this->extractParts($match, $url);
 
-				// USE SMARTED METHOD!
+				// @todo USE SMARTED METHOD!
 				foreach ($params as &$param) {
 					$param = $param[0];
 				}
 
 				$result = $this->buildAction($action, $params ?? []);
-			}	
+			}
 		}
 
 		return $result;
 	}
 
 	/**
+	 * Builds an action based on routes and current url
 	 *
 	 * @todo Extract this to Parser / SyntaxTransformer
-	 * @param string $parts Associative array of pretty string version of the action (route syntax) and parameter keys of the route
-	 * @param array $params parameters we want to pass along
-	 * @return array Action array with controller, method and parameters ordered nicely
+	 * @param string $parts 	Associative array of pretty string version of the
+	 *							action (route syntax) and parameter keys of the route
+	 * @param array $params 	Parameters we want to pass along
+	 * @return array 			Action array with controller, method and
+	 *							parameters ordered nicely
 	 */
 	protected function buildAction(array $action, $params = []) : array
 	{
@@ -206,7 +189,7 @@ class RouteMatcher
 			// filter empty parameters
 			if (!isset($params[$i]) || empty($params[$i])) continue;
 
-			$paramKey = $action['paramKeys'][$i] ?? 'unknown';
+			$paramKey = $action['paramNames'][$i] ?? 'unknown';
 			$paramArr[$paramKey] = $params[$i];
 
 		}
@@ -218,17 +201,60 @@ class RouteMatcher
 		];
 	}
 
+
+	/**---------------------------------------------
+	 * Methods From Here are REGEX and GRAMMAR stuff
+	 *--------------------------------------------*/
+
+
 	/**
-	 * Find and extract certain ocurrences from a string to an array
+	 * Find and extract certain ocurrences from a string to an array.
+	 * Also removes the 'full-match' array.
 	 * 
-	 * @param $match The needle
-	 * @param $subject The haystack
-	 * @return array Array of needles
+	 * @param $match 	The needle
+	 * @param $subject 	The haystack
+	 * @return array 	Array of the picked needles
 	 */
 	protected function extractParts(string $match, string $subject) : array
 	{
 		preg_match_all($match, $subject, $needles);
 		return array_unset($needles) ?? [];
+	}
+
+	/**
+	 * Returns the method defined in the given route string
+	 *
+	 * @param string $url
+	 * @return string
+	 */
+	protected function getMethodFromRoute(string $route) : string
+	{
+		preg_match_all($this->regex('method'), $route, $results);
+
+		if (isset($results[1]) && isset($results[1][0])) {	
+			return  $this->getMethodName($results[1][0]);
+		}
+
+		throw new InternalException("Method missing from route definiton {$route}.");
+	}
+
+	/**
+	 * Returns the method name by key.
+	 *
+	 * @param string $key Key of the method
+	 * @return mixed 
+	 */
+	protected function getMethodName(string $key)
+	{
+		$methods = [
+			'&' => 'GET',
+			'$' => 'POST'
+		];
+
+		if(array_key_exists($key, $methods))
+			return $methods[$key];
+		else
+			return false;
 	}
 
 	/**
@@ -253,5 +279,18 @@ class RouteMatcher
 	protected function optionalizeSlashes(string $url) : string
 	{
 		return preg_replace('/(\/)(?=\{\?)/', '[/]?', $url);
+	}
+
+	/**
+	 * Checks if two urls are matching
+	 *
+	 * @param string $match 	The first url we will compare to another one
+	 * @param string $url 		The another url
+	 * @param string $method 	The method of the url
+	 * @return bool
+	 */
+	public function isMatchingUrl(string $match, string $url, string $method) : bool
+	{
+		return preg_match($match, $url) && $this->method == $method;
 	}
 }
