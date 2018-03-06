24829c8c27c913f1680c2586ec4443734f5da4e7
Miika Sikala <miikasikala96@gmail.com>
2018-03-06 15:34:30 +0200

    Request getters and setters

--- app/Framework/Libs/Request.php
+++ app/Framework/Libs/Request.php
@@ -2,17 +2,55 @@
 
 namespace App\Framework\Libs;
 
+use App\Framework\Exceptions\InternalException;
+
+/**
+ * This class is not in real use. It's just an experimental thing!
+ * Yeshh.
+ */
 class Request
 {
-	public function __construct()
+	protected $params = [];
+
+	/**
+	 * Sets array of parameters as the object's properties.
+	 * 
+	 * NOT IN USE!
+	 *
+	 * @param array $params
+	 * @return void
+	 */
+	public function setParams(array $params) : void
 	{
-		//
+		foreach ($params as $key => $value) { 
+			$this->__set($key, $value);
+		}
 	}
 
-	public function setParams(array $params)
+	/**
+	 * Set a single parameter
+	 *
+	 * @param string $ey 
+	 * @param $value 
+	 * @return void
+	 */
+	public function __set(string $key, $value) : void
 	{
-		foreach ($params as $key => $value) {
-			$this->$key = $value;
+		$this->params[$key] = $value;
+	}
+
+	/**
+	 * Get a single parameter
+	 *
+	 * @param string $param
+	 * @return $param
+	 **/
+	public function __get(string $key)
+	{
+		if (array_key_exists($key, $this->params)) {
+			return $this->params[$key];
 		}
+
+		throw new InternalException("Parameter {$param} not found.");
 	}
 }
