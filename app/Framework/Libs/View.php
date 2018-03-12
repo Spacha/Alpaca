6b45c6391d71c97969b62feb49343fa7f0a09e93
Miika Sikala <miikasikala96@gmail.com>
2018-03-12 23:56:26 +0200

    Simple snippets

--- app/Framework/Libs/View.php
+++ app/Framework/Libs/View.php
@@ -4,13 +4,16 @@ namespace App\Framework\Libs;
 
 class View
 {
+	protected $snippets = [];
 	protected $template = '';
 	protected $data = [];
 
-	public function __construct($template, $data = [])
+	public function __construct($template, $data = [], $snippets = [])
 	{
 		$this->template = $this->templatePath($template);
+		$this->setSnippets($snippets);
 		$this->data = $data;
+
 		$this->render();
 	}
 
@@ -18,7 +21,32 @@ class View
 	{
 		extract($this->data);
 
+		$snippets = $this->snippets;
+
 		require $this->template;
+	}
+
+	public function setSnippets(array $snippets)
+	{
+		$this->snippets += $this->getSnippets($snippets);
+	}
+
+	protected function getSnippets(array $snippets) : array
+	{
+		$result = [];
+
+		foreach($snippets as $snippet) {
+			ob_start();
+			require app_path('views', '_snippets/'.$snippet.'.phtml');
+			$result[$snippet] = ob_get_clean();
+		}
+
+		return $result;
+	}
+
+	protected function layoutPath(string $layout) : string
+	{
+		return app_path('views', "_layouts/{$layout}.phtml");
 	}
 
 	protected function templatePath(string $template) : string
