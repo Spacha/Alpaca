2c379748f82560962a025b90750c21b0985a487d
Spacha <miikasikala96@gmail.com>
2019-06-08 17:32:52 +0300

    Fixed lowercase letter in dbConfig

---
+++ app/example.dbConfig.php
@@ -0,0 +1,17 @@
+<?php
+
+/*---------------------------------------------------------
+ * Database config file
+ *---------------------------------------------------------
+ *
+ * Change name to dbconfig.php when in use.
+ */
+
+return [
+	'connection'		=> 'mysql:host=127.0.0.1',
+	'name' 				=> 'alpaca',
+	'user' 				=> 'root',
+	'password' 			=> '',
+	'table_prefix' 		=> '',
+	'options'			=> []
+];
