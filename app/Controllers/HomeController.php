99ebbca722755138e72fae675f8118633bbf0871
Spacha <miikasikala96@gmail.com>
2021-05-20 02:50:56 +0300

    Front page improvements

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -12,10 +12,20 @@ use App\Models\Test;
 
 class HomeController extends Controller
 {
+	const SLOGANS = [
+		"It works on my machine.",
+		"Have you tried turning it off and on again?",
+		"It's a beautiful day, isn't it.",
+		"Hello, stranger. Nice to meet you.",
+		"I'm bad with slogans.",
+		"To the Moon!"
+	];
+
 	public function home()
 	{
 		return new View('home.home', [
 			'active' => 'home',
+			'slogan' => self::SLOGANS[array_rand(self::SLOGANS)],
 			'topSection' => true
 		], ['header', 'footer']);
 	}
