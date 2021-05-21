242b26e34df53d1a27b66e8ec66ce4073353b7f5
Spacha <miikasikala96@gmail.com>
2021-05-21 03:38:07 +0300

    Production assets

--- public/js/after-dom.js
+++ public/js/after-dom.js
@@ -1,15 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!************************************!*\
-  !*** ./app/assets/js/after-dom.js ***!
-  \************************************/
-function fadeInSlogan() {
-  var elem = document.querySelector('.slogan');
-  elem.classList.remove('before-fading');
-} ////////////////////////////////////////////////////////////////////////////////
-// Run after DOM has laoded
-
-
-fadeInSlogan();
-/******/ })()
-;
+document.querySelector(".slogan").classList.remove("before-fading");
