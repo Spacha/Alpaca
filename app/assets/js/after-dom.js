582b3f7470847df6e93dc2b9826ef8ccf13237b4
Spacha <miikasikala96@gmail.com>
2021-05-21 03:06:06 +0300

    Fun transitions, script after DOM

---
+++ app/assets/js/after-dom.js
@@ -0,0 +1,10 @@
+function fadeInSlogan()
+{
+	var elem = document.querySelector('.slogan');
+	elem.classList.remove('before-fading');
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Run after DOM has laoded
+fadeInSlogan();
---
+++ public/js/after-dom.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************************!*\
+  !*** ./app/assets/js/after-dom.js ***!
+  \************************************/
+function fadeInSlogan() {
+  var elem = document.querySelector('.slogan');
+  elem.classList.remove('before-fading');
+} ////////////////////////////////////////////////////////////////////////////////
+// Run after DOM has laoded
+
+
+fadeInSlogan();
+/******/ })()
+;
