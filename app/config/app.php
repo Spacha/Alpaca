a86a3c5145b189aa7ec6d6d1d53f7e58115a7f3f
Spacha <miikasikala96@gmail.com>
2022-04-04 09:30:50 +0300

    Formatted email notifier message

--- app/config/app.php
+++ app/config/app.php
@@ -12,7 +12,7 @@ return [
 	'name' => 'Spacha',
 
 	// Application environment (development / production)
-	'env' => 'production',
+	'env' => 'development',
 
 	// Default timezone
 	'timezone' => 'Europe/Helsinki',
